380daf5b2a2a3371e7f08e39f6ca17a629e8b3f1
Namit Doshi <namitdoshi90@gmail.com>
2019-10-14 22:05:23 +0530

    code refactored

--- js/jquery.cccheck.js
+++ js/jquery.cccheck.js
@@ -6,67 +6,69 @@
  * by Ruan Aragão
  */
 
- function getCCLabel(cc_number_mask) {
+function getCCLabel(cc_number_mask) {
+  var regexVisa = /^4[0-9]{5}(?:[0-9]{3})?/; // 12
+  var regexMaster = /^5[1-5][0-9]{4}/; // 14
+  var regexAmex = /^3[47][0-9]{4}/; // 13
+  var regexDiners = /^3(?:0[0-5]|[68][0-9])[0-9]{3}/; // 11
+  var regexDiscover = /^6(?:011|5[0-9]{2})[0-9]{4}/; // 12
+  var regexJCB = /^(?:2131|1800|35\d{3})\d{3}/; // 11
+  var regexHiper = /^(?:38|60)\d{4}/;
+  var regexElo = /^(?:636368|636369|438935|504175|451416|636297|5067|4576|4011|506699)\d{4}/;
 
-    var regexVisa = /^4[0-9]{5}(?:[0-9]{3})?/; // 12
-    var regexMaster = /^5[1-5][0-9]{4}/; // 14
-    var regexAmex = /^3[47][0-9]{4}/; // 13
-    var regexDiners = /^3(?:0[0-5]|[68][0-9])[0-9]{3}/; // 11
-    var regexDiscover = /^6(?:011|5[0-9]{2})[0-9]{4}/; // 12
-    var regexJCB = /^(?:2131|1800|35\d{3})\d{3}/; // 11
-    var regexHiper = /^(?:38|60)\d{4}/;
-    var regexElo = /^(?:636368|636369|438935|504175|451416|636297|5067|4576|4011|506699)\d{4}/;
+  cc_number = cc_number_mask.replace(/-/gi, "");
 
-    cc_number = cc_number_mask.replace(/-/gi, '');
+  if (regexVisa.test(cc_number)) {
+    return "visa";
+  }
+  if (regexMaster.test(cc_number)) {
+    return "master";
+  }
+  if (regexAmex.test(cc_number)) {
+    return "amex";
+  }
+  if (regexDiners.test(cc_number)) {
+    return "diners";
+  }
+  if (regexDiscover.test(cc_number)) {
+    return "discover";
+  }
+  if (regexJCB.test(cc_number)) {
+    return "jcb";
+  }
+  if (regexHiper.test(cc_number)) {
+    return "hipercard";
+  }
+  if (regexElo.test(cc_number)) {
+    return "elo";
+  }
 
-    if(regexVisa.test(cc_number)) {
-        return 'visa';
-    }
-    if(regexMaster.test(cc_number)) {
-        return 'master';
-    }
-    if(regexAmex.test(cc_number)) {
-        return 'amex';
-    }
-    if(regexDiners.test(cc_number)) {
-        return 'diners';
-    }
-    if(regexDiscover.test(cc_number)) {
-        return 'discover';
-    }
-    if(regexJCB.test(cc_number)) {
-        return 'jcb';
-    }
-    if(regexHiper.test(cc_number)) {
-        return 'hipercard';
-    }
-    if(regexElo.test(cc_number)) {
-        return 'elo';
-    }
+  return false;
+}
 
-    return false;
-
- }
-
-;(function ( $, window, document, undefined ) {
-    $.cccheck = function(settings){
-        var config = {
-            // Config local
-            'input_cc_number'     :   '#cc-number',
-            'element_show_label'  :   '#show-cc-label'
-        };
-        if (settings){settings = jQuery.extend(config, settings);}
-
-        // variables
-        var nodeNumber  = $(config.input_cc_number);
-        var nodeLabel   = $(config.element_show_label);
-
-        nodeNumber.keyup(function(e) {
-            card_label = getCCLabel(nodeNumber.val());
-            nodeLabel.html('<i class="i-icon-payment i-icon-card-' + card_label + '"></i>');
-            //console.log(card_label);
-        });
-
-        return this;
+(function($, window, document, undefined) {
+  $.cccheck = function(settings) {
+    var config = {
+      // Config local
+      input_cc_number: "#cc-number",
+      element_show_label: "#show-cc-label"
     };
-})( jQuery, window, document );
+    if (settings) {
+      settings = jQuery.extend(config, settings);
+    }
+
+    // variables
+    var nodeNumber = $(config.input_cc_number);
+    var nodeLabel = $(config.element_show_label);
+
+    nodeNumber.keyup(function(e) {
+      card_label = getCCLabel(nodeNumber.val());
+      nodeLabel.html(
+        '<i class="i-icon-payment i-icon-card-' + card_label + '"></i>'
+      );
+      //console.log(card_label);
+    });
+
+    return this;
+  };
+})(jQuery, window, document);
